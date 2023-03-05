f28b651f16d1e6e163c5ec6f13c71207f90487b5
Jibin Mathew Peechatt <jibin@email.com>
2023-03-06 00:00:50 +0100

    Completed version of controller

--- src/week2/MVC/calculator/CalculatorController.java
+++ src/week2/MVC/calculator/CalculatorController.java
@@ -1,14 +1,39 @@
 package week2.MVC.calculator;
 
-public class CalculatorController {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
+public class CalculatorController implements EventHandler<ActionEvent> {
 	private CalculatorModel model;
 	private CalculatorView view;
 	
-
 	public CalculatorController(CalculatorModel model, CalculatorView view) {
 		this.model = model;
 		this.view = view;
 		
+		for (Button b : view.digits) {
+			b.setOnAction(this);
+		}
+		view.plus.setOnAction(this);
+		view.clear.setOnAction(this);
+		view.equalto.setOnAction(this);
+		view.minus.setOnAction(this);
 	}
 
+@Override
+public void handle(ActionEvent event) {
+	Button btn = (Button) event.getSource();
+	
+	if (btn == view.clear) {
+		view.txt.setText("");
+	}
+	else if (btn == view.equalto) {
+		String result = model.calculate(view.txt.getText());
+		view.txt.setText(result);
+	}else { 
+		view.txt.setText(view.txt.getText() + btn.getText());
+	}
+}
 }
