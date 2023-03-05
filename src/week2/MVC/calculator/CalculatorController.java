45a0a79f42ae2e2dc71527b32ed48ab924f0fe26
Jibin Mathew Peechatt <jibin@email.com>
2023-03-05 16:15:09 +0100

    Basic setup done

---
+++ src/week2/MVC/calculator/CalculatorController.java
@@ -0,0 +1,14 @@
+package week2.MVC.calculator;
+
+public class CalculatorController {
+	private CalculatorModel model;
+	private CalculatorView view;
+	
+
+	public CalculatorController(CalculatorModel model, CalculatorView view) {
+		this.model = model;
+		this.view = view;
+		
+	}
+
+}
