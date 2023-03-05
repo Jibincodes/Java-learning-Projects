fe8110610f3248efd407b329722e3d6769817fd9
Jibin Mathew Peechatt <jibin@email.com>
2023-03-05 15:53:27 +0100

    Setting up the main class

---
+++ src/week2/MVC/calculator/Calculator.java
@@ -0,0 +1,24 @@
+package week2.MVC.calculator;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Calculator extends Application {
+	private CalculatorModel model;
+	private CalculatorView view;
+	private CalculatorController controller;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		model = new CalculatorModel();
+		view = new CalculatorView(primaryStage,model);
+		controller = new CalculatorController(model, view);
+		view.start();
+	}
+
+	public static void main(String[] args) {
+		launch();
+
+	}
+
+}
