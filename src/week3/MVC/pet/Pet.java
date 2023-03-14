4fed985fb23c2704a445ec66d445fc3043568a90
Jibin Mathew Peechatt <jibin@email.com>
2023-03-14 23:14:46 +0100

    Final version of Main class

---
+++ src/week3/MVC/pet/Pet.java
@@ -0,0 +1,26 @@
+package week3.MVC.pet;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class Pet extends Application {
+	private PetView view;
+	private PetModel model;
+	private PetController controller;
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		model = new PetModel();
+		view = new PetView(stage, model);
+		controller = new PetController(model, view);
+		view.start();
+
+	}
+
+	public static void main(String[] args) {
+		launch();
+
+	}
+
+}
