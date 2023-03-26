ddf2a387d50b6d8be251f1da65be9c3b7ac884f8
Jibin Mathew Peechatt <jibin@email.com>
2023-03-26 14:59:11 +0200

    Completed Controller

---
+++ src/week3/MVC/pet/PetController.java
@@ -0,0 +1,48 @@
+package week3.MVC.pet;
+
+import javafx.event.ActionEvent;
+
+public class PetController {
+	private PetView view;
+	private PetModel model;
+
+	public PetController(PetModel model, PetView view) {
+		this.model = model;
+		this.view = view;
+		
+		view.btnSave.setOnAction(this::save);
+		view.btnDelete.setOnAction(this::delete);
+		
+		view.btnDelete.disableProperty().bind(model.petProperty().isNull());
+		view.btnSave.disableProperty().bind(view.txtName.textProperty().isEmpty());
+	}
+	private void save(ActionEvent e) {
+		week3.MVC.pet.Pet1.Species species = view.cmbSpecies.getValue();
+		week3.MVC.pet.Pet1.Gender gender = view.cmbGender.getValue();
+		String name = view.txtName.getText();
+		if (species != null && gender != null && name != null && name.length() > 0) {
+			model.savePet(species, gender, name);
+			updateView(model.getPet());
+		}
+	}
+	
+	private void delete(ActionEvent e) {
+		model.deletePet();
+		updateView(model.getPet());
+	}
+	
+	private void updateView(Pet1 pet1) {
+		if (pet1 != null) {
+			view.lblDataID.setText(Integer.toString(pet1.getID()));
+			view.lblDataName.setText(pet1.getName());
+			view.lblDataSpecies.setText(pet1.getSpecies().toString());
+			view.lblDataGender.setText(pet1.getGender().toString());
+		} else {
+			view.lblDataID.setText("");
+			view.lblDataName.setText("");
+			view.lblDataSpecies.setText("");
+			view.lblDataGender.setText("");
+		}
+	}
+
+}
