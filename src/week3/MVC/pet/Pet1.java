0976cea697fd72a6bd863555b8942171f12a53e3
Jibin Mathew Peechatt <jibin@email.com>
2023-03-26 14:58:46 +0200

    The setup class via question

---
+++ src/week3/MVC/pet/Pet1.java
@@ -0,0 +1,57 @@
+package week3.MVC.pet;
+
+// taken from the solution class for usage and this program is being extended as per as the question
+public class Pet1 {
+	public enum Species {CAT, DOG, HORSE};
+	public enum Gender {MALE, FEMALE};
+	
+	private static int highestID = -1;
+	
+	private final int ID;
+	private Species species;
+	private Gender gender;
+	private String name;
+
+	// Class method to get next available ID
+	private static int getNextID() {
+		return ++highestID;
+	}
+	
+	public Pet1(Species species, Gender gender, String name) {
+		this.ID = getNextID();
+		this.species = species;
+		this.gender = gender;
+		this.name = name;
+	}
+	
+	// --- Getters and Setters ---
+
+	public Species getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(Species species) {
+		this.species = species;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+}
