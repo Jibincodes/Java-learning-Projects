a8f379b505896b08436108ac120b106d7073bc45
Jibin Mathew Peechatt <jibin@email.com>
2023-05-18 02:14:57 +0200

    Just testing for Find function

--- src/week9/DataStructures/ListTestingController.java
+++ src/week9/DataStructures/ListTestingController.java
@@ -25,13 +25,35 @@ public class ListTestingController {
 		Integer amountOfData = view.cmbNumElements.getValue();
 		COLLECTION_TYPES collectionType = view.cmbListType.getValue(); // here too
 		WHERE_CHOICES whereChoice = view.cmbWhere.getValue();
+		SampleData selectedObject = view.getSelectedObject();
 
 		float runTime = model.runTest(amountOfData, collectionType, whereChoice);
-		
 		view.lblResult.setText("Time: " + timeFormatter.format(runTime) + " seconds");
-		
+		//runTime = model.runFindTest(amountOfData, collectionType);
+		float runtime2 = model.runFindTest(amountOfData, collectionType, selectedObject);
+		view.lblResult1.setText("Time: " + timeFormatter.format(runtime2) + " seconds");
 		// Trigger garbage collection
 		System.gc();
 	}
+	/*private void setUpTest(ActionEvent e) {
+	    // Get selections from View
+	    int amountOfData = Integer.parseInt(view.cmbNumElements.getValue());
+	    ListTestingModel.COLLECTION_TYPES collectionType = view.cmbListType.getValue();
+	    ListTestingModel.WHERE_CHOICES whereChoice = view.cmbWhere.getValue();
+	    SampleData selectedObject = view.getSelectedObject(); // Assuming there's a method to get the selected object from the view
+
+	    float runTime = 0.0f;
+	    if (view.rbAdd.isSelected()) {
+	        runTime = model.runAddTest(amountOfData, collectionType, whereChoice);
+	    } else if (view.rbFind.isSelected()) {
+	        runTime = model.runFindTest(amountOfData, collectionType, selectedObject);
+	    }
+
+	    view.lblResult.setText("Time: " + timeFormatter.format(runTime) + " seconds");
+
+	    // Trigger garbage collection
+	    System.gc();
+	}*/
+
 
 }
