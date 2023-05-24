1889994afeecdbb9be79a21a61a0a57f154dbefb
Jibin Mathew Peechatt <jibin@email.com>
2023-05-24 02:41:51 +0200

    New changes for the finding function

--- src/week9/DataStructures/ListTestingController.java
+++ src/week9/DataStructures/ListTestingController.java
@@ -20,7 +20,7 @@ public class ListTestingController {
 		view.btnGo.setOnAction(this::setUpTest);
 	}
 	
-	private void setUpTest(ActionEvent e) {
+	/*private void setUpTest(ActionEvent e) {
 		// Get selections from View
 		Integer amountOfData = view.cmbNumElements.getValue();
 		COLLECTION_TYPES collectionType = view.cmbListType.getValue(); // here too
@@ -34,6 +34,25 @@ public class ListTestingController {
 		view.lblResult1.setText("Time: " + timeFormatter.format(runtime2) + " seconds");
 		// Trigger garbage collection
 		System.gc();
+	}*/
+	private void setUpTest(ActionEvent e) {
+	    // Get selections from View
+	    Integer amountOfData = view.cmbNumElements.getValue();
+	    COLLECTION_TYPES collectionType = view.cmbListType.getValue();
+	    WHERE_CHOICES whereChoice = view.cmbWhere.getValue();
+	    SampleData selectedObject = view.getSelectedObject(); // Retrieve the selected object
+
+	    float runTime = model.runTest(amountOfData, collectionType, whereChoice);
+	    view.lblResult.setText("Time: " + timeFormatter.format(runTime) + " seconds");
+
+	    // Run the find test using the selected object
+	    if (selectedObject != null) {
+	        float findRunTime = model.runFindTest(amountOfData, collectionType, selectedObject);
+	        view.lblResult1.setText("Find Time: " + timeFormatter.format(findRunTime) + " seconds");
+	    }
+
+	    // Trigger garbage collection
+	    System.gc();
 	}
 	/*private void setUpTest(ActionEvent e) {
 	    // Get selections from View
