53b8fd946b9deff84b4b3d489fc475355b8f5914
Jibin Mathew Peechatt <jibin@email.com>
2023-05-31 01:33:13 +0200

    adding the requested operations

--- src/week10/SinglyLinkedList/SinglyLinkedList.java
+++ src/week10/SinglyLinkedList/SinglyLinkedList.java
@@ -2,6 +2,8 @@ package week10.SinglyLinkedList;
 
 import week10.SinglyLinkedList.ListException.ListErrorCode;
 
+
+
 public class SinglyLinkedList<T> {
 	private ListElement<T> head = null;
 
@@ -10,7 +12,23 @@ public class SinglyLinkedList<T> {
 	}
 
 	public void add(int position, T o) throws ListException {
-		// TODO: Add code from lecture here
+		ListElement<T> e = new ListElement<T>(o);
+		if (position == 0) { // special handling for first position
+			e.next = head;
+			head = e;
+		} else if (position > 0) {
+			ListElement<T> cursor = head;			
+			for (int i = 1; i < position; i++) {
+				if (cursor == null) throw new ListException(ListErrorCode.PositionTooSmall);
+				cursor = cursor.next;
+			}
+			// insert *after* element pointed to by cursor
+			if (cursor == null) throw new ListException(ListErrorCode.PositionTooLarge);
+			e.next = cursor.next;
+			cursor.next = e;
+		} else {
+			throw new ListException(ListErrorCode.PositionTooSmall);
+		}
 	}
 
 	public T get(int position) throws ListException {
@@ -25,17 +43,49 @@ public class SinglyLinkedList<T> {
 	}
 
 	public T remove(int position) throws ListException {
-		// TODO: Exercise
-		return null;
+		T o;
+		ListElement<T> cursor = head;
+		if (position == 0) { // special handling for first position
+			if (cursor == null) throw new ListException(ListErrorCode.PositionTooLarge);
+			o = cursor.getData();
+			head = cursor.next;
+		} else if (position > 0) {
+			for (int i = 1; i < position; i++) {
+				if (cursor == null) throw new ListException(ListErrorCode.PositionTooLarge);
+				cursor = cursor.next;
+			}
+			if (cursor == null) throw new ListException(ListErrorCode.PositionTooLarge);
+			// retain data to return
+			o = cursor.next.getData();
+			// delete element *after* cursor
+			cursor.next = cursor.next.next;
+		} else {
+			throw new ListException(ListErrorCode.PositionTooSmall);
+		}
+		return o;
 	}
 	
 	public int size() {
-		// TODO: Exercise
-		return 0;
+		int count = 0;
+		ListElement<T> cursor = head;
+		while (cursor != null) {
+			count++;
+			cursor = cursor.next;
+		}
+		return count;
 	}
 	
 	public boolean contains(T elt) {
-		// TODO: Exercise
-		return false;
+		boolean found = false;
+		ListElement<T> cursor = head;
+		while (!found && cursor != null) {
+			if (elt == null) {
+				found = (cursor.getData() == null);
+			} else {
+				found = (cursor.getData().equals(elt));
+			}
+			cursor = cursor.next;
+		}
+		return found;
 	}
 }
